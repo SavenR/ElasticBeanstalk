71f813057071c0e69b17490750c107b7eabee875
Saven Roybal <SavenR@users.noreply.github.com>
2015-05-28 11:50:29 -0700

    Updated local settings

--- local_settings.py
+++ local_settings.py
@@ -7,7 +7,6 @@ DATABASES = {
           # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
      # This should be whichever database that you want to use
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
     'NAME': 'app',
 
      }
